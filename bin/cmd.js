ea938456b1fee83332819a79506b392d89311966
Feross Aboukhadijeh <feross@feross.org>
2015-01-29 15:22:54 -0800

    Add --help flag

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,16 +1,41 @@
 #!/usr/bin/env node
 
+var minimist = require('minimist')
 var standard = require('../')
 
-var flag = process.argv[2]
+var argv = minimist(process.argv.slice(2), {
+  boolean: [ 'help', 'bare', 'verbose', 'version' ],
+  alias: {
+    help: 'h',
+    verbose: 'v'
+  }
+})
 
-if (flag === '--version') {
+if (argv.help) {
+  console.log(function () {
+  /*
+  Usage:
+      standard <flags>
+
+  Flags:
+      -v, --verbose    Show error codes (so you can ignore specific rules)
+          --version    Display the current version
+      -h, --help       Display the help and usage details
+
+  Report bugs:  https://github.com/feross/standard/issues
+
+  */
+  }.toString().split(/\n/).slice(2, -2).join('\n'))
+  process.exit(0)
+}
+
+if (argv.version) {
   console.log(require('../package.json').version)
   process.exit(0)
 }
 
 standard({
   cwd: process.cwd(),
-  bare: flag === '--bare' || flag === '-b',
-  verbose: flag === '--verbose' || flag === '-v'
+  bare: argv.bare, // undocumented: show raw, unprocessed linter output (for debugging)
+  verbose: argv.verbose
 })
