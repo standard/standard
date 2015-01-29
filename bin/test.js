3d4db4f9646d8e0015a4ae1c0cfdec8f9c71d17a
Feross Aboukhadijeh <feross@feross.org>
2015-01-29 00:19:11 -0800

    tests: obey tested project's ignore files
    :)

--- bin/test.js
+++ bin/test.js
@@ -8,15 +8,17 @@
  */
 
 var cp = require('child_process')
+var extend = require('extend.js')
 var mkdirp = require('mkdirp')
 var path = require('path')
 var rimraf = require('rimraf')
 var series = require('run-series')
+var parallel = require('run-parallel')
 
 var TMP = path.join(__dirname, '..', 'tmp')
 var STANDARD = path.join(__dirname, 'cmd.js')
 
-var URLS = [
+var urls = [
   'https://github.com/feross/webtorrent.git',
   'https://github.com/feross/bittorrent-tracker.git',
   'https://github.com/feross/bittorrent-dht.git',
@@ -24,26 +26,39 @@ var URLS = [
   // 'https://github.com/mafintosh/level-temp.git'
 ]
 
+var modules = {}
+urls.forEach(function (url) {
+  var name = /\/([^.\/]+)\.git$/.exec(url)[1]
+  modules[name] = url
+})
+
 rimraf.sync(TMP)
 mkdirp.sync(TMP)
 
-series(URLS.map(function (url) {
+series(Object.keys(modules).map(function (name) {
+  var url = modules[name]
   return function (cb) {
-    var name = /\/([^.\/]+)\.git$/.exec(url)[1]
     var args = [ 'clone', url, path.join(TMP, name) ]
     // TODO: Start `git` in a way that works on Windows – PR welcome!
-    spawn('git', args, cb)
+    spawn('git', args, {}, cb)
   }
-}), function (err) {
+}), runTests)
+
+function runTests (err) {
   if (err) return error(err)
-  spawn(STANDARD, ['--verbose'], function (err) {
+  parallel(Object.keys(modules).map(function (name) {
+    return function (cb) {
+      var cwd = path.join(TMP, name)
+      spawn(STANDARD, ['--verbose'], { cwd: cwd }, cb)
+    }
+  }), function (err) {
     if (err) return error(err)
     console.log('ok')
   })
-})
+}
 
-function spawn (command, args, cb) {
-  var child = cp.spawn(command, args, { stdio: 'inherit' })
+function spawn (command, args, opts, cb) {
+  var child = cp.spawn(command, args, extend({ stdio: 'inherit' }, opts))
   child.on('error', error)
   child.on('close', function (code) {
     if (code !== 0) cb(new Error('non-zero exit code: ' + code))
