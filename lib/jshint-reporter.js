standard/standard

commit 3afa5e5dfe8578f161092291a1f4e42a56ef164c

Use ESLint
Using ESLint helps us detect many more errors and potential bugs,
including:
- When the `err` param is not handled
- When switch statements are missing a default case
- When variables are used *before* definition (hoisting bug)
- When there is unreachable code (ex: after a `return`)

Since this adds new rules, this is a breaking change (major version
bump).

Thanks to @Raynos for the tip about ESLint!

lib/jshint-reporter.js
@@ -1,0 +1,19 @@
+module.exports = {
+  reporter: function (results, data, opts) {
+    var str = ''
+    opts = opts || {}
+
+    results.forEach(function (result) {
+      var file = result.file
+      var error = result.error
+
+      str += file + ':' + error.line + ':' + error.character + ': ' + error.reason
+
+      if (opts.verbose) {
+        str += ' (' + error.code + ')'
+      }
+
+      str += '\n'
+    })
+  }
+}

bin/test.js
@@ -35,7 +35,7 @@ series(URLS.map(function (url) {
   }
 }), function (err) {
   if (err) return error(err)
-  spawn(STANDARD, [], function (err) {
+  spawn(STANDARD, ['--verbose'], function (err) {
     if (err) return error(err)
     console.log('ok')
   })

index.js
@@ -6,12 +6,18 @@ var path = require('path')
 var split = require('split')
 
 var JSHINT = path.join(__dirname, 'node_modules', '.bin', 'jshint')
-var JSHINTRC = path.join(__dirname, 'rc', '.jshintrc')
+var JSHINT_RC = path.join(__dirname, 'rc', '.jshintrc')
+var JSHINT_REPORTER = path.join(__dirname, 'lib', 'jshint-reporter.js')
 
 var JSCS = path.join(__dirname, 'node_modules', '.bin', 'jscs')
-var JSCSRC = path.join(__dirname, 'rc', '.jscsrc')
+var JSCS_RC = path.join(__dirname, 'rc', '.jscsrc')
 var JSCS_REPORTER = path.join(__dirname, 'lib', 'jscs-reporter.js')
 var JSCS_REPORTER_VERBOSE = path.join(__dirname, 'lib', 'jscs-reporter-verbose.js')
+
+var ESLINT = path.join(__dirname, 'node_modules', '.bin', 'eslint')
+var ESLINT_RC = path.join(__dirname, 'rc', '.eslintrc')
+var ESLINT_REPORTER = path.join(__dirname, 'lib', 'eslint-reporter.js')
+var ESLINT_REPORTER_VERBOSE = path.join(__dirname, 'lib', 'eslint-reporter-verbose.js')
 
 if (/^win/.test(process.platform)) {
   JSHINT += '.cmd'
@@ -61,10 +67,11 @@ module.exports = function (opts) {
       })
     })
 
-    var jshintArgs = ['--config', JSHINTRC, '--reporter', 'unix']
-
+    var jshintArgs = ['--config', JSHINT_RC, '--reporter', JSHINT_REPORTER]
     var jscsReporter = opts.verbose ? JSCS_REPORTER_VERBOSE : JSCS_REPORTER
-    var jscsArgs = ['--config', JSCSRC, '--reporter', jscsReporter, '--esnext']
+    var jscsArgs = ['--config', JSCS_RC, '--reporter', jscsReporter]
+    var eslintReporter = opts.verbose ? ESLINT_REPORTER_VERBOSE : ESLINT_REPORTER
+    var eslintArgs = ['--config', ESLINT_RC, '--format', eslintReporter]
 
     if (opts.verbose) {
       jshintArgs.push('--verbose')
@@ -73,10 +80,13 @@ module.exports = function (opts) {
 
     jshintArgs = jshintArgs.concat(files)
     jscsArgs = jscsArgs.concat(files)
+    eslintArgs = eslintArgs.concat(files)
 
     spawn(JSHINT, jshintArgs, function (err1) {
       spawn(JSCS, jscsArgs, function (err2) {
-        done(err1 || err2)
+        spawn(ESLINT, eslintArgs, function (err3) {
+          done(err1 || err2 || err3)
+        })
       })
     })
   })
@@ -98,7 +108,6 @@ module.exports = function (opts) {
       .pipe(split())
       .on('data', function (line) {
         if (line === '') return
-        if (/^\d+ errors?/.test(line)) return
         errors.push(line)
       })
   }

lib/eslint-reporter-verbose.js
@@ -1,0 +1,20 @@
+module.exports = function (results) {
+  var output = ''
+
+  results.forEach(function (result) {
+    var messages = result.messages
+    messages.forEach(function (message) {
+      var msg = message.message
+      if (msg[msg.length - 1] === '.') msg = msg.substring(0, msg.length - 1)
+
+      output += result.filePath
+      output += ':' + message.line || 0
+      output += ':' + message.column || 0
+      output += ': ' + msg
+      output += message.ruleId ? ' (' + message.ruleId + ')' : ''
+      output += '\n'
+    })
+  })
+
+  return output
+}

lib/eslint-reporter.js
@@ -1,0 +1,19 @@
+module.exports = function (results) {
+  var output = ''
+
+  results.forEach(function (result) {
+    var messages = result.messages
+    messages.forEach(function (message) {
+      var msg = message.message
+      if (msg[msg.length - 1] === '.') msg = msg.substring(0, msg.length - 1)
+
+      output += result.filePath
+      output += ':' + message.line || 0
+      output += ':' + message.column || 0
+      output += ': ' + msg
+      output += '\n'
+    })
+  })
+
+  return output
+}

lib/jscs-reporter-verbose.js
@@ -1,16 +1,13 @@
 var util = require('util')
 module.exports = function (errorsCollection) {
-  var errorCount = 0
   errorsCollection.forEach(function (errors) {
     var file = errors.getFilename()
-    if (!errors.isEmpty()) {
-      errors.getErrorList().forEach(function (error) {
-        errorCount++
-        var message = error.message.substring(error.rule.length + 2)
-        console.log(util.format(
-          '%s:%d:%d: %s (%s)', file, error.line, error.column, message, error.rule
-        ))
-      })
-    }
+    if (errors.isEmpty()) return
+    errors.getErrorList().forEach(function (error) {
+      var message = error.message.substring(error.rule.length + 2)
+      console.log(util.format(
+        '%s:%d:%d: %s (%s)', file, error.line, error.column, message, error.rule
+      ))
+    })
   })
 }

lib/jscs-reporter.js
@@ -1,15 +1,12 @@
 var util = require('util')
 module.exports = function (errorsCollection) {
-  var errorCount = 0
   errorsCollection.forEach(function (errors) {
     var file = errors.getFilename()
-    if (!errors.isEmpty()) {
-      errors.getErrorList().forEach(function (error) {
-        errorCount++
-        console.log(util.format(
-          '%s:%d:%d: %s', file, error.line, error.column, error.message
-        ))
-      })
-    }
+    if (errors.isEmpty()) return
+    errors.getErrorList().forEach(function (error) {
+      console.log(util.format(
+        '%s:%d:%d: %s', file, error.line, error.column, error.message
+      ))
+    })
   })
 }

rc/.eslintrc
@@ -1,0 +1,148 @@
+{
+    "env": {
+        "browser": true,
+        "node": true,
+        "amd": false,
+        "mocha": false,
+        "jasmine": false
+    },
+
+    "globals": {
+        "chrome": false,
+        "crypto": true,
+        "escape": false,
+        "FileList": false,
+        "unescape": false
+    },
+
+    "rules": {
+        "no-alert": 2,
+        "no-array-constructor": 2,
+        "no-bitwise": 0,
+        "no-caller": 2,
+        "no-catch-shadow": 0,
+        "no-comma-dangle": 2,
+        "no-cond-assign": 2,
+        "no-constant-condition": 2,
+        "no-control-regex": 2,
+        "no-debugger": 2,
+        "no-delete-var": 2,
+        "no-div-regex": 0,
+        "no-dupe-keys": 2,
+        "no-else-return": 0,
+        "no-empty": 0,
+        "no-empty-class": 2,
+        "no-empty-label": 2,
+        "no-eq-null": 0,
+        "no-eval": 2,
+        "no-ex-assign": 2,
+        "no-extend-native": 2,
+        "no-extra-bind": 2,
+        "no-extra-boolean-cast": 2,
+        "no-extra-parens": 0,
+        "no-extra-semi": 2,
+        "no-extra-strict": 2,
+        "no-fallthrough": 2,
+        "no-floating-decimal": 2,
+        "no-func-assign": 2,
+        "no-implied-eval": 2,
+        "no-inline-comments": 0,
+        "no-inner-declarations": [2, "functions"],
+        "no-invalid-regexp": 2,
+        "no-irregular-whitespace": 2,
+        "no-iterator": 2,
+        "no-label-var": 2,
+        "no-labels": 2,
+        "no-lone-blocks": 2,
+        "no-lonely-if": 2,
+        "no-loop-func": 0,
+        "no-mixed-requires": [0, false],
+        "no-mixed-spaces-and-tabs": [2, false],
+        "no-multi-spaces": 2,
+        "no-multi-str": 2,
+        "no-multiple-empty-lines": [2, {"max": 1}],
+        "no-native-reassign": 2,
+        "no-negated-in-lhs": 2,
+        "no-nested-ternary": 0,
+        "no-new": 2,
+        "no-new-func": 2,
+        "no-new-object": 2,
+        "no-new-require": 2,
+        "no-new-wrappers": 2,
+        "no-obj-calls": 2,
+        "no-octal": 2,
+        "no-octal-escape": 2,
+        "no-path-concat": 0,
+        "no-plusplus": 0,
+        "no-process-exit": 0,
+        "no-proto": 2,
+        "no-redeclare": 2,
+        "no-regex-spaces": 2,
+        "no-reserved-keys": 0,
+        "no-restricted-modules": 0,
+        "no-return-assign": 2,
+        "no-script-url": 2,
+        "no-self-compare": 2,
+        "no-sequences": 2,
+        "no-shadow": 0,
+        "no-shadow-restricted-names": 2,
+        "no-space-before-semi": 0,
+        "no-spaced-func": 2,
+        "no-sparse-arrays": 2,
+        "no-sync": 0,
+        "no-ternary": 0,
+        "no-trailing-spaces": 2,
+        "no-undef": 2,
+        "no-undef-init": 2,
+        "no-undefined": 0,
+        "no-underscore-dangle": 0,
+        "no-unreachable": 2,
+        "no-unused-expressions": 0,
+        "no-unused-vars": 0,
+        "no-use-before-define": [2, "nofunc"],
+        "no-void": 0,
+        "no-warning-comments": [0, { "terms": ["todo", "fixme", "xxx"], "location": "start" }],
+        "no-with": 2,
+        "no-wrap-func": 2,
+        "block-scoped-var": 0,
+        "brace-style": 0,
+        "camelcase": 0,
+        "comma-spacing": [2, {"before": false, "after": true}],
+        "comma-style": [2, "last"],
+        "consistent-return": 0,
+        "consistent-this": [2, "self"],
+        "curly": 0,
+        "default-case": 2,
+        "dot-notation": 2,
+        "eol-last": 2,
+        "eqeqeq": 2,
+        "func-style": [0, "declaration"],
+        "guard-for-in": 0,
+        "handle-callback-err": [2, "^(err|error|anySpecificError)$" ],
+        "key-spacing": [2, { "beforeColon": false, "afterColon": true }],
+        "new-cap": [2, { "newIsCap": true, "capIsNew": false }],
+        "new-parens": 2,
+        "one-var": 0,
+        "operator-assignment": [0, "always"],
+        "padded-blocks": 0,
+        "quote-props": 0,
+        "quotes": [2, "single"],
+        "radix": 2,
+        "semi": [2, "never"],
+        "sort-vars": 0,
+        "space-after-function-name": [2, "always"],
+        "space-after-keywords": [2, "always"],
+        "space-before-blocks": [2, "always"],
+        "space-infix-ops": 2,
+        "space-return-throw-case": 2,
+        "space-unary-ops": [2, { "words": true, "nonwords": false }],
+        "spaced-line-comment": [2, "always"],
+        "strict": 0,
+        "use-isnan": 2,
+        "valid-typeof": 2,
+        "vars-on-top": 0,
+        "wrap-iife": 2,
+        "wrap-regex": 0,
+        "yoda": [2, "never"]
+    }
+}

commit 728ae35a356d2b6f001750fede460ddee98c8c75

fix jshint reporter not logging errors
Introduced in 3afa5e5

lib/jshint-reporter.js
@@ -1,19 +1,14 @@
 module.exports = {
   reporter: function (results, data, opts) {
-    var str = ''
     opts = opts || {}
 
     results.forEach(function (result) {
       var file = result.file
       var error = result.error
 
-      str += file + ':' + error.line + ':' + error.character + ': ' + error.reason
-
-      if (opts.verbose) {
-        str += ' (' + error.code + ')'
-      }
-
-      str += '\n'
+      var str = file + ':' + error.line + ':' + error.character + ': ' + error.reason
+      if (opts.verbose) str += ' (' + error.code + ')'
+      console.log(str)
     })
   }
 }