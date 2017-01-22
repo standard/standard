4f42a43588466916bcf4b65639d351fad8b7cfdf
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2017-01-23 00:01:32 +0200

    some work on getConfig

--- get-config.js
+++ get-config.js
@@ -7,8 +7,35 @@ const noSet = {
   set: () => false
 }
 
-const handlers = {
-  config: {
+const setToPush = {
+  set: (target, property, value) => {
+    target.push(value)
+    return true
+  }
+}
+
+const getConfig = () => {
+  const config = {
+    bugs: pkg.bugs.url,
+    name: 'standard',
+    homepage: pkg.homepage,
+    tagline: 'Use JavaScript Standard Style',
+    version: pkg.version,
+    defaultFiles: new Proxy(files, noSet),
+    ignore: new Proxy(ignore, setToPush),
+    gitIgnore: true,
+    packageJson: true,
+    CLIEngineOptions: new Proxy({
+      configFile: path.join(__dirname, 'eslintrc.json'),
+      globals: new Proxy([], setToPush)
+    }, noSet)
+  }
+
+  Object.assign(config, {
+    globals: config.CLIEngineOptions.globals
+  })
+
+  return new Proxy(config, {
     set: (target, property, value) => {
       const allow = [
         'cwd'
@@ -20,24 +47,7 @@ const handlers = {
         return false
       }
     }
-  },
-  CLIEngineOptions: noSet,
-  defaultFiles: noSet,
-  ignore: noSet
+  })
 }
-
-const getConfig = () => new Proxy({
-  bugs: pkg.bugs.url,
-  name: 'standard',
-  homepage: pkg.homepage,
-  tagline: 'Use JavaScript Standard Style',
-  version: pkg.version,
-  defaultFiles: new Proxy(files, handlers.defaultFiles),
-  ignore: new Proxy(ignore, handlers.ignore),
-  gitIgnore: true,
-  CLIEngineOptions: new Proxy({
-    configFile: path.join(__dirname, 'eslintrc.json')
-  }, handlers.CLIEngineOptions)
-}, handlers.config)
 
 module.exports = getConfig
