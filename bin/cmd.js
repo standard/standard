d8a3e25c604ce4e3894b3e315ea0afd085ce16ef
Feross Aboukhadijeh <feross@feross.org>
2019-08-11 18:32:32 -0700

    Fix: compatibility with old Node.js versions that lack `const`

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,6 +3,5 @@
 if (process.version.match(/v(\d+)\./)[1] < 8) {
   console.error('standard: Node 8 or greater is required. `standard` did not run.')
 } else {
-  const opts = require('../options')
-  require('standard-engine').cli(opts)
+  require('standard-engine').cli(require('../options'))
 }
