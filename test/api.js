9fe2f40b751ce41ee72f5f83c5d5489a0b8f50f6
Feross Aboukhadijeh <feross@feross.org>
2019-08-11 17:06:30 -0700

    Use modern ES style
    Supersedes https://github.com/standard/standard/pull/1198 by @jimmywarting. Thanks for the handy lebab command!

--- test/api.js
+++ test/api.js
@@ -1,18 +1,18 @@
-var standard = require('../')
-var test = require('tape')
+const standard = require('../')
+const test = require('tape')
 
-test('api: lintFiles', function (t) {
+test('api: lintFiles', t => {
   t.plan(3)
-  standard.lintFiles(['bin/cmd.js'], {}, function (err, result) {
+  standard.lintFiles(['bin/cmd.js'], {}, (err, result) => {
     t.error(err, 'no error while linting')
     t.equal(typeof result, 'object', 'result is an object')
     t.equal(result.errorCount, 0)
   })
 })
 
-test('api: lintText', function (t) {
+test('api: lintText', t => {
   t.plan(3)
-  standard.lintText('console.log("hi there")\n', function (err, result) {
+  standard.lintText('console.log("hi there")\n', (err, result) => {
     t.error(err, 'no error while linting')
     t.equal(typeof result, 'object', 'result is an object')
     t.equal(result.errorCount, 1, 'should have used single quotes')
--- test/clone.js
+++ test/clone.js
@@ -6,22 +6,22 @@
  * VERSION BUMP.)
  */
 
-var crossSpawn = require('cross-spawn')
-var fs = require('fs')
-var minimist = require('minimist')
-var mkdirp = require('mkdirp')
-var os = require('os')
-var parallelLimit = require('run-parallel-limit')
-var path = require('path')
-var standardPackages = require('standard-packages')
-var test = require('tape')
+const crossSpawn = require('cross-spawn')
+const fs = require('fs')
+const minimist = require('minimist')
+const mkdirp = require('mkdirp')
+const os = require('os')
+const parallelLimit = require('run-parallel-limit')
+const path = require('path')
+const standardPackages = require('standard-packages')
+const test = require('tape')
 
-var GIT = 'git'
-var STANDARD = path.join(__dirname, '..', 'bin', 'cmd.js')
-var TMP = path.join(__dirname, '..', 'tmp')
-var PARALLEL_LIMIT = Math.ceil(os.cpus().length / 2)
+const GIT = 'git'
+const STANDARD = path.join(__dirname, '..', 'bin', 'cmd.js')
+const TMP = path.join(__dirname, '..', 'tmp')
+const PARALLEL_LIMIT = Math.ceil(os.cpus().length / 2)
 
-var argv = minimist(process.argv.slice(2), {
+const argv = minimist(process.argv.slice(2), {
   boolean: [
     'disabled',
     'offline',
@@ -31,12 +31,12 @@ var argv = minimist(process.argv.slice(2), {
   ]
 })
 
-var testPackages = argv.quick
+let testPackages = argv.quick
   ? standardPackages.test.slice(0, 20)
   : standardPackages.test
 
-var disabledPackages = []
-testPackages = testPackages.filter(function (pkg) {
+const disabledPackages = []
+testPackages = testPackages.filter(pkg => {
   if (pkg.disable) disabledPackages.push(pkg)
   return !pkg.disable
 })
@@ -44,61 +44,61 @@ testPackages = testPackages.filter(function (pkg) {
 if (argv.disabled) {
   testPackages = disabledPackages
 } else {
-  test('Disabled Packages', function (t) {
-    disabledPackages.forEach(function (pkg) {
-      console.log('DISABLED: ' + pkg.name + ': ' + pkg.disable + ' (' + pkg.repo + ')')
+  test('Disabled Packages', t => {
+    disabledPackages.forEach(pkg => {
+      console.log(`DISABLED: ${pkg.name}: ${pkg.disable} (${pkg.repo})`)
     })
     t.end()
   })
 }
 
-test('test github repos that use `standard`', function (t) {
+test('test github repos that use `standard`', t => {
   t.plan(testPackages.length)
 
   mkdirp.sync(TMP)
 
-  parallelLimit(testPackages.map(function (pkg) {
-    var name = pkg.name
-    var url = pkg.repo + '.git'
-    var folder = path.join(TMP, name)
-    return function (cb) {
-      fs.access(path.join(TMP, name), fs.R_OK | fs.W_OK, function (err) {
+  parallelLimit(testPackages.map(pkg => {
+    const name = pkg.name
+    const url = `${pkg.repo}.git`
+    const folder = path.join(TMP, name)
+    return cb => {
+      fs.access(path.join(TMP, name), fs.R_OK | fs.W_OK, err => {
         if (argv.offline && err) {
-          t.pass('SKIPPING (offline): ' + name + ' (' + pkg.repo + ')')
+          t.pass(`SKIPPING (offline): ${name} (${pkg.repo})`)
           cb(null)
         } else if (argv.offline) {
           runStandard(cb)
         } else {
-          var downloadPackage = err ? gitClone : gitPull
-          downloadPackage(function (err) {
+          const downloadPackage = err ? gitClone : gitPull
+          downloadPackage(err => {
             if (err) return cb(err)
             runStandard(cb)
           })
         }
 
         function gitClone (cb) {
-          var args = ['clone', '--depth', 1, url, path.join(TMP, name)]
-          spawn(GIT, args, { stdio: 'ignore' }, function (err) {
-            if (err) err.message += ' (git clone) (' + name + ')'
+          const args = ['clone', '--depth', 1, url, path.join(TMP, name)]
+          spawn(GIT, args, { stdio: 'ignore' }, err => {
+            if (err) err.message += ` (git clone) (${name})`
             cb(err)
           })
         }
 
         function gitPull (cb) {
-          var args = ['pull']
-          spawn(GIT, args, { cwd: folder, stdio: 'ignore' }, function (err) {
-            if (err) err.message += ' (git pull) (' + name + ')'
+          const args = ['pull']
+          spawn(GIT, args, { cwd: folder, stdio: 'ignore' }, err => {
+            if (err) err.message += ` (git pull) (${name})`
             cb(err)
           })
         }
 
         function runStandard (cb) {
-          var args = [STANDARD, '--verbose']
-          if (pkg.args) args.push.apply(args, pkg.args)
-          spawn('node', args, { cwd: folder }, function (err) {
-            var str = name + ' (' + pkg.repo + ')'
+          const args = [STANDARD, '--verbose']
+          if (pkg.args) args.push(...pkg.args)
+          spawn('node', args, { cwd: folder }, err => {
+            const str = `${name} (${pkg.repo})`
             if (err && argv.fix) {
-              t.comment('Attempting --fix on ' + str)
+              t.comment(`Attempting --fix on ${str}`)
               runStandardFix(cb)
             } else if (err) {
               t.fail(str)
@@ -111,26 +111,26 @@ test('test github repos that use `standard`', function (t) {
         }
 
         function runStandardFix (cb) {
-          var args = [STANDARD, '--fix', '--verbose']
-          if (pkg.args) args.push.apply(args, pkg.args)
-          spawn('node', args, { cwd: folder }, function (err) {
-            var str = name + ' (' + pkg.repo + ') ** with --fix'
+          const args = [STANDARD, '--fix', '--verbose']
+          if (pkg.args) args.push(...pkg.args)
+          spawn('node', args, { cwd: folder }, err => {
+            const str = `${name} (${pkg.repo}) ** with --fix`
             if (err) { t.fail(str) } else { t.pass(str) }
             runGitReset(cb)
           })
         }
 
         function runGitReset (cb) {
-          var args = ['reset', '--hard']
-          spawn(GIT, args, { cwd: folder }, function (err) {
-            if (err) err.message += ' (git reset) (' + name + ')'
+          const args = ['reset', '--hard']
+          spawn(GIT, args, { cwd: folder }, err => {
+            if (err) err.message += ` (git reset) (${name})`
             // Fatal error if can't git reset
             cb(err)
           })
         }
       })
     }
-  }), PARALLEL_LIMIT, function (err) {
+  }), PARALLEL_LIMIT, err => {
     if (err) throw err
   })
 })
@@ -138,10 +138,10 @@ test('test github repos that use `standard`', function (t) {
 function spawn (command, args, opts, cb) {
   if (!opts.stdio) opts.stdio = argv.quiet ? 'ignore' : 'inherit'
 
-  var child = crossSpawn(command, args, opts)
+  const child = crossSpawn(command, args, opts)
   child.on('error', cb)
-  child.on('close', function (code) {
-    if (code !== 0) return cb(new Error('non-zero exit code: ' + code))
+  child.on('close', code => {
+    if (code !== 0) return cb(new Error(`non-zero exit code: ${code}`))
     cb(null)
   })
   return child
