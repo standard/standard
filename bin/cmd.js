b3a4916bfd4d3b2658bde310b82dc15236153d3f
Linus Unnebäck <linus@folkdatorn.se>
2019-11-24 14:54:24 +0000

    Add support for Node.js 8.3 - 8.6
    The spread operator was actually introduced in Node.js 8.3

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,8 +4,8 @@ var match = process.version.match(/v(\d+)\.(\d+)/)
 var major = parseInt(match[1], 10)
 var minor = parseInt(match[2], 10)
 
-if (major >= 9 || (major === 8 && minor >= 6)) {
+if (major >= 9 || (major === 8 && minor >= 3)) {
   require('standard-engine').cli(require('../options'))
 } else {
-  console.error('standard: Node 8 or greater is required. `standard` did not run.')
+  console.error('standard: Node 8.3 or greater is required. `standard` did not run.')
 }
