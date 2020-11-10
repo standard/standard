d456d05a7032444d2e6c3a43c22a4dc367443429
alanclarke <alan@qubit.com>
2020-11-10 13:05:00 +0000

    Support node 11

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,7 +5,7 @@ var match = process.version.match(/v(\d+)\.(\d+)/)
 var major = parseInt(match[1], 10)
 var minor = parseInt(match[2], 10)
 
-if (major >= 12 || (major === 10 && minor >= 12)) {
+if (major >= 11 || (major === 10 && minor >= 12)) {
   require('standard-engine').cli(require('../options'))
 } else {
   console.error('standard: Node 10.12.0 or greater is required. `standard` did not run.')
