ad9d4eedf7ed009a7129cb3201398398f85a2b55
Jimmy Wärting <jimmy@warting.se>
2021-09-14 11:43:04 +0200

    revert variable name

--- test/api.js
+++ test/api.js
@@ -1,9 +1,9 @@
 import test from 'tape'
-import linter from '../index.js'
+import standard from '../index.js'
 
 test('api: lintFiles', t => {
   t.plan(3)
-  linter.lintFiles(['bin/cmd.js'], {}, (err, result) => {
+  standard.lintFiles(['bin/cmd.js'], {}, (err, result) => {
     t.error(err, 'no error while linting')
     t.equal(typeof result, 'object', 'result is an object')
     t.equal(result.errorCount, 0)
@@ -12,7 +12,7 @@ test('api: lintFiles', t => {
 
 test('api: lintText', t => {
   t.plan(3)
-  linter.lintText('console.log("hi there")\n', (err, result) => {
+  standard.lintText('console.log("hi there")\n', (err, result) => {
     t.error(err, 'no error while linting')
     t.equal(typeof result, 'object', 'result is an object')
     t.equal(result.errorCount, 1, 'should have used single quotes')
