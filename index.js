a9c26a2ee2aa9f046740e0342676599cd3ba1fb4
Jimmy Wärting <jimmy@warting.se>
2021-09-14 11:47:07 +0200

    rename opts to options

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 /*! standard. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
 import engine from 'standard-engine'
-import opts from './options.js'
+import options from './options.js'
 
-const Linter = engine.linter
-
-export default new Linter(opts)
+export default new engine.linter(options)
